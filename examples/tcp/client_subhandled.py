e14a896cc12e3b432fecbf37e648b71a1e2c636e
oubiwann <>
2006-05-07 09:19:06 +0000

    * Commented out the support for returning a dict with keys "result" and "error" as it seems like unessary overhead for now. Someone convince me otherwise, and I'll put it back in. * Changed methodology for adding subhandlers. * Fixed introspection.

---
+++ examples/tcp/client_subhandled.py
@@ -0,0 +1,47 @@
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+
+from twisted.internet import reactor
+from twisted.internet import defer
+
+try:
+    from adytum.twisted.jsonrpc import Proxy
+except ImportError:
+    from pkg_resources import require
+    require('Twisted-JSONRPC')
+    from adytum.twisted.jsonrpc import Proxy
+
+def printValue(value):
+    print "Result: %s" % str(value)
+
+def printError(error):
+    print 'error', error
+
+def shutDown(data):
+    print "Shutting down reactor..."
+    reactor.stop()
+
+print "Making remote calls..."
+proxy = Proxy('127.0.0.1', 7080)
+dl = []
+
+d = proxy.callRemote('system.listMethods')
+d.addCallbacks(printValue, printError)
+dl.append(d)
+
+d = proxy.callRemote('echo', 'bite me')
+d.addCallbacks(printValue, printError)
+dl.append(d)
+
+d = proxy.callRemote('testing.getList')
+d.addCallbacks(printValue, printError)
+dl.append(d)
+
+d = proxy.callRemote('math.add', 3, 5)
+d.addCallbacks(printValue, printError)
+dl.append(d)
+
+dl = defer.DeferredList(dl)
+dl.addCallback(shutDown)
+reactor.run()

1033b9f49b47d7da0f01cdc8ca2b6dac2279c759
Duncan McGreggor <oubiwann@gmail.com>
2008-06-15 01:50:06 -0500

    2008.06.15
    * Added Fault class.
    * Updated SimpleParser to properly return results when using jsonrpclib
    (synchronous, blocking code).
    * Added Transport and ServerProxy.

--- txjsonrpc/jsonrpclib.py
+++ txjsonrpc/jsonrpclib.py
@@ -15,9 +15,11 @@ INVALID_JSONRPC       = xmlrpclib.INVALID_XMLRPC
 METHOD_NOT_FOUND      = xmlrpclib.METHOD_NOT_FOUND
 INVALID_METHOD_PARAMS = xmlrpclib.INVALID_METHOD_PARAMS
 INTERNAL_ERROR        = xmlrpclib.INTERNAL_ERROR
-Fault                 = xmlrpclib.Fault
 # Custom errors
 METHOD_NOT_CALLABLE   = -32604
+
+class Fault(xmlrpclib.Fault):
+    pass
 
 class NoSuchFunction(Fault):
     """
@@ -42,6 +44,7 @@ def loads(string, **kws):
 class SimpleParser(object):
 
     def feed(self, data):
+        print data
         self.data = loads(data)
 
     def close(self):
@@ -53,11 +56,47 @@ class SimpleUnmarshaller(object):
         return self.parser.data.get("method")
 
     def close(self):
-        return self.parser.data.get("params")
+        if isinstance(self.parser.data, dict):
+            return self.parser.data.get("params")
+        return self.parser.data
 
 def getparser():
     parser = SimpleParser()
     marshaller = SimpleUnmarshaller()
     marshaller.parser = parser
     return parser, marshaller
+
+class Transport(xmlrpclib.Transport):
+    """
+    Handles an HTTP transaction to an XML-RPC server.
+    """
+    user_agent = "jsonrpclib.py (by txJSON-RPC)"
+
+    def getparser(self):
+        """
+        Get Parser and unmarshaller.
+        """
+        return getparser()
+
+class ServerProxy(xmlrpclib.ServerProxy):
+    """
+
+    """
+    def __init__(self, uri, transport=Transport(), *args, **kwds):
+        xmlrpclib.ServerProxy.__init__(self, uri, transport, *args, **kwds)
+
+    def __request(self, method, args):
+        """
+        Call a method on the remote server.
+        """
+        request = dumps({'method':method, 'params':args})
+        response = self.__transport.request(
+            self.__host,
+            self.__handler,
+            request,
+            verbose=self.__verbose
+            )
+        if len(response) == 1:
+            response = response[0]
+        return response
 

fffbee9dc6f344baa0989ec37fd8ad790f8adbd3
Duncan McGreggor <oubiwann@gmail.com>
2009-10-20 11:54:59 -0600

    2009.11.20
    * Added clean.sh admin script.
    * Cleaned up TODO file.
    * Updated meta file with more data and incremented the version to 0.3.
    * Added utils subpackage.
    * Updated setup.py to use the new utils subpackage.
    * Added docs.
    * Updated README.

---
+++ admin/clean.sh
@@ -0,0 +1,4 @@
+. ./admin/defs.sh
+
+find . -name "*pyc" -exec rm {} \;
+cleanup
--- txjsonrpc/meta.py
+++ txjsonrpc/meta.py
@@ -1,1 +1,8 @@
-version = "0.2.0"
+display_name = "txJSON-RPC"
+library_name = "txjsonrpc"
+version = "0.3"
+author = "Duncan McGreggor"
+author_email = "oubiwann@adytum.us"
+license = "BSD, GPL"
+url = "http://launchpad.net/%s" % library_name
+description = "Code for creatig Twisted JSON-RPC servers and clients."

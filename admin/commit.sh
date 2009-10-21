1b471e91570096596eaa679fb86e002adbf3fd3e
Duncan McGreggor <oubiwann@gmail.com>
2009-10-20 20:57:25 -0600

    * Added manifest file. * Updated the commit script to run the doctest runner.

--- admin/commit.sh
+++ admin/commit.sh
@@ -10,11 +10,14 @@ else
     # send the output (stdout and stderr) to both a file for checking and
     # stdout for immediate viewing/feedback purposes
     ./admin/runExamples.py || error " ERROR: examples failed"
+    ./admin/testDocs.py 2>&1|tee doctest.out
     trial $LIB 2>&1|tee test.out
 fi
-STATUS=`tail -1 test.out|grep 'FAIL'`
-if [[ "$STATUS" == '' ]];then
-    if [[ "$1" == "FLAG" ]];then
+TEST_STATUS=`tail -1 test.out`
+DOCTEST_STATUS=`tail -1 doctest.out`
+STATUS=`echo "$TEST_STATUS $DOCTEST_STATUS"|grep 'FAIL'`
+if [[ -z "$STATUS" ]]; then
+    if [[ "$1" == "FLAG" ]]; then
         echo "Skipping tests..."
     else
         echo "All tests passed."

09a14dfc74d1d4dce6eb121f6db88b1dbc44ff1d
Alexandre Bezroutchko <abb@gremwell.com>
2012-06-23 17:07:36 +0200

    more

--- examples/ssl/client.py
+++ examples/ssl/client.py
@@ -1,5 +1,9 @@
+import logging
 from twisted.internet import reactor, ssl
 from txjsonrpc.web.jsonrpc import Proxy
+
+from OpenSSL import SSL
+from twisted.python import log
 
 def printValue(value):
     print "Result: %s" % str(value)
@@ -11,15 +15,31 @@ def shutDown(data):
     print "Shutting down reactor..."
     reactor.stop()
 
+def verifyCallback(connection, x509, errnum, errdepth, ok):
+    log.msg(connection.__str__())
+    if not ok:
+        log.msg('invalid server cert: %s' % x509.get_subject(), logLevel=logging.ERROR)
+        return False
+    else:
+        log.msg('good server cert: %s' % x509.get_subject(), logLevel=logging.INFO)
+        return True
+
 class AltCtxFactory(ssl.ClientContextFactory):
     def getContext(self):
         #self.method = SSL.SSLv23_METHOD
         ctx = ssl.ClientContextFactory.getContext(self)
+        ctx.set_verify(SSL.VERIFY_PEER, verifyCallback)
+        ctx.load_verify_locations("cacert.pem")
         #ctx.use_certificate_file('keys/client.crt')
         #ctx.use_privatekey_file('keys/client.key')
         return ctx
 
-proxy = Proxy('https://127.0.0.1:7443/', ssl_ctx_factory=AltCtxFactory)
+
+import sys
+log.startLogging(sys.stdout)
+
+#proxy = Proxy('https://127.0.0.1:7443/', ssl_ctx_factory=AltCtxFactory)
+proxy = Proxy('https://127.0.0.2:7443/', ssl_ctx_factory=AltCtxFactory)
 
 d = proxy.callRemote('add', 3, 5)
 d.addCallback(printValue).addErrback(printError).addBoth(shutDown)
